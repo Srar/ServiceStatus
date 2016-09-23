8a3b9ecadfbec5091b12423ab8829b33ef7a6e14
Srar <a1020938070@gmail.com>
2016-09-23 20:59:52 +0800

    完善Tcp连接检测服务

--- src/services/TcpConnect.js
+++ src/services/TcpConnect.js
@@ -1,13 +1,61 @@
 /* @flow */
 
+import net from "net";
 import SerivceReportModel from "../models/SerivceReportModel.js";
+import type { ServiceTaskModel } from "../models/ServiceTaskModel.js";
+
+function _test(host, port, timeout): Promise<SerivceReportModel> {
+    return new Promise((success) => {
+         var startTime: number = Date.now();
+
+        var client = new net.Socket();
+        client.setTimeout(timeout);
+        client.connect({
+            host: host,
+            port: port
+        }, function () {
+            try {
+                client.end();
+            } catch (error) { }
+            try {
+                client.destroy();
+            } catch (error) { }
+            var report = new SerivceReportModel(true);
+            report.setMessage("time", (Date.now() - startTime).toString());
+            success(report);
+        });
+        client.on('error', function (err) {
+            try {
+                client.destroy();
+            } catch (error) { }
+            var report = new SerivceReportModel(false);
+            report.setMessage("error", err.toString());
+            success(report);
+        });
+    });
+}
 
 export default class TcpConnect {
-    reportStatus(target: any): SerivceReportModel {
-        return new SerivceReportModel(false);
+    async reportStatus(taskModel: ServiceTaskModel): Promise<SerivceReportModel> {
+        var host: string = "";
+        var port: number = -1;
+
+        if (taskModel.Target["host"] == undefined) {
+            console.error(`[TcpConnect] target host undefined.`);
+            return new SerivceReportModel(false);
+        }
+        if (taskModel.Target["port"] == undefined) {
+            console.error(`[TcpConnect] target port undefined.`);
+            return new SerivceReportModel(false);
+        }
+
+        host = taskModel.Target["host"];
+        port = parseInt(taskModel.Target["port"]);
+
+       return await _test(host, port, 5 * 1000);
     }
 
     getCheckTimerLimit(): number {
-        return 7;
+        return 5;
     }
 }
